8aea9df12a1acc36a4b1c3077685c77d87417f5c
erasels <emanuelbedburdick@yahoo.de>
2020-12-12 12:22:24 +0100

    Initial commit

---
+++ OptimizeTheSpire/src/main/java/OptimizeTheSpire/OptimizeTheSpire.java
@@ -0,0 +1,53 @@
+package OptimizeTheSpire;
+
+import OptimizeTheSpire.patches.RelicOptimizationPatches;
+import basemod.BaseMod;
+import basemod.interfaces.PostBattleSubscriber;
+import basemod.interfaces.PostInitializeSubscriber;
+import basemod.interfaces.PreStartGameSubscriber;
+import basemod.interfaces.RelicGetSubscriber;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static OptimizeTheSpire.patches.MiscPatches.logsSkipped;
+import static OptimizeTheSpire.patches.RelicOptimizationPatches.*;
+
+@SpireInitializer
+public class OptimizeTheSpire implements
+        PostInitializeSubscriber,
+        RelicGetSubscriber,
+        PostBattleSubscriber,
+        PreStartGameSubscriber{
+    public static final Logger optimizationLogger = LogManager.getLogger(OptimizeTheSpire.class);
+
+    public static void initialize() {
+        BaseMod.subscribe(new OptimizeTheSpire());
+    }
+
+    @Override
+    public void receivePostInitialize() {
+
+    }
+
+    @Override
+    public void receiveRelicGet(AbstractRelic abstractRelic) {
+        RelicOptimizationPatches.doReceiveRelic(abstractRelic);
+    }
+
+    @Override
+    public void receivePostBattle(AbstractRoom abstractRoom) {
+        optimizationLogger.info("Unneeded element iterations skipped: " + rIterSkipped);
+        optimizationLogger.info("Logs prevented: " + logsSkipped);
+        if(wrongPosition > 0) {
+            optimizationLogger.info("Attempted accesses to wrong relic positions: " + wrongPosition);
+        }
+    }
+
+    @Override
+    public void receivePreStartGame() {
+        relicLocs.clear();
+    }
+}
---
+++ OptimizeTheSpire/src/main/java/OptimizeTheSpire/patches/RelicOptimizationPatches.java
@@ -0,0 +1,73 @@
+package OptimizeTheSpire.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import java.util.HashMap;
+
+public class RelicOptimizationPatches {
+    //Optimize relic access
+    public static int rIterSkipped = 0;
+    public static int wrongPosition = 0;
+    public static HashMap<String, Integer> relicLocs = new HashMap<>();
+
+    @SpirePatch(clz = AbstractPlayer.class, method = "getRelic")
+    public static class OptimizeGetRelic {
+        @SpirePrefixPatch
+        public static SpireReturn<AbstractRelic> patch(AbstractPlayer __instance, String key) {
+            AbstractRelic rel;
+            Integer i = relicLocs.get(key);
+            if (i != null) {
+                if (i != -1) {
+                    if (i < __instance.relics.size() && __instance.relics.get(i).relicId.equals(key)) {
+                        rIterSkipped += i - 1;
+                        return SpireReturn.Return(__instance.relics.get(i));
+                    } else {
+                        wrongPosition++;
+                        rel = manualGetRelic(__instance, key);
+                        if(rel != null) {
+                            return SpireReturn.Return(rel);
+                        }
+                    }
+                } else {
+                    rIterSkipped += __instance.relics.size();
+                }
+            } else {
+                rel = manualGetRelic(__instance, key);
+                //may return null but that's valid
+                return SpireReturn.Return(rel);
+            }
+            return SpireReturn.Return(null);
+        }
+    }
+
+    @SpirePatch(clz = AbstractPlayer.class, method = "hasRelic")
+    public static class OptimizeHasRelic {
+        @SpirePrefixPatch
+        public static SpireReturn<Boolean> patch(AbstractPlayer __instance, String key) {
+            SpireReturn<AbstractRelic> sp = OptimizeGetRelic.patch(__instance, key);
+            return SpireReturn.Return(sp.get() != null);
+        }
+    }
+
+    public static void doReceiveRelic(AbstractRelic r) {
+        //Relic hasn't been added yet so setting position as size directly functions as last position + 1
+        relicLocs.put(r.relicId, AbstractDungeon.player.relics.size());
+    }
+
+    public static AbstractRelic manualGetRelic(AbstractPlayer p, String key) {
+        for (int j = 0; j < p.relics.size(); j++) {
+            AbstractRelic r = p.relics.get(j);
+            if (r.relicId.equals(key)) {
+                relicLocs.put(key, j);
+                return r;
+            }
+        }
+        relicLocs.put(key, -1);
+        return null;
+    }
+}
